47185341a21fed18c3efabd8b7d4314aa84290db
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-16 21:13:00 -0500

    .

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ function burguerWrapper () {
     document.querySelector('.burguer, .close, .nav').classList.toggle('active'); 
 }
 function buttonWrapper () {
-    document.querySelector('.button, body').classList.toggle('dark');
+    document.querySelector('.button').classList.toggle('dark');
+    document.querySelector('body').classList.toggle('dark');
 }
- 
+function estilo1 () {
+    document.getElementById('style-1').classList.toggle('estilo1')
+}
