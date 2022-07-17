e12910ace7de0264158c2b4a477a769bd1a070fb
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-16 19:19:35 -0500

    Menu test

---
+++ app.js
@@ -0,0 +1,7 @@
+function burguerWrapper () {
+    document.querySelector('.burguer, .close, .nav').classList.toggle('active'); 
+}
+function buttonWrapper () {
+    document.querySelector('.button').classList.toggle('dark');
+}
+ 
