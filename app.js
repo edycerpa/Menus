20e4968f02be86b0b6d41df04561740426ec57dd
Eddy Cerpa <82135358+edycerpa@users.noreply.github.com>
2022-07-17 00:47:05 -0500

    .

--- app.js
+++ app.js
@@ -6,5 +6,26 @@ function buttonWrapper () {
     document.querySelector('body').classList.toggle('dark');
 }
 function estilo1 () {
-    document.getElementById('style-1').classList.toggle('estilo1')
+    document.getElementById('style-1').classList.remove('hide')
+    document.querySelector('#style-2').classList.add('hide')
+    document.querySelector('#style-3').classList.add('hide')
+    document.querySelector('#style-4').classList.add('hide')
+}
+function estilo2 () {
+    document.getElementById('style-1').classList.add('hide')
+    document.querySelector('#style-2').classList.remove('hide')
+    document.querySelector('#style-3').classList.add('hide')
+    document.querySelector('#style-4').classList.add('hide')
+}
+function estilo3 () {
+    document.getElementById('style-1').classList.add('hide')
+    document.querySelector('#style-2').classList.add('hide')
+    document.querySelector('#style-3').classList.remove('hide')
+    document.querySelector('#style-4').classList.add('hide')
+}
+function estilo4 () {
+    document.getElementById('style-1').classList.add('hide')
+    document.querySelector('#style-2').classList.add('hide')
+    document.querySelector('#style-3').classList.add('hide')
+    document.querySelector('#style-4').classList.remove('hide')
 }
